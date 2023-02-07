529971902e81b4fcb2e7b5fd4c4fdef1f7f4e861
JangHyenSeo <wkdgustj02@naver.com>
2023-02-07 17:51:11 +0900

    1

--- src/test/java/com/iu/home/bankBook/BankBookDAOTest.java
+++ src/test/java/com/iu/home/bankBook/BankBookDAOTest.java
@@ -12,13 +12,13 @@ public class BankBookDAOTest {
 	@Autowired
 	private BankBookDAO bankBookDAO;
 	
-	@Test
-	public void getBankBookList() throws Exception {
-		List<BankBookDTO> ar = bankBookDAO.getBankBookList();
-		
-		assertNotEquals(0, ar.size());
-	}
-	
+//	@Test
+//	public void getBankBookList() throws Exception {
+//		List<BankBookDTO> ar = bankBookDAO.getBankBookList();
+//		
+//		assertNotEquals(0, ar.size());
+//	}
+//	
 //	@Test
 //	public void getBankBookDetail() throws Exception {
 //		BankBookDTO bankBookDTO = new BankBookDTO();
@@ -29,4 +29,19 @@ public class BankBookDAOTest {
 //		
 //		assertNotNull(bankBookDTO);
 //	}
+	
+	@Test
+	public void setBankBookAdd() throws Exception {
+		BankBookDTO bankBookDTO = new BankBookDTO();
+		
+		bankBookDTO.setBookNumber(22L);
+		bankBookDTO.setBookName("HS");
+		bankBookDTO.setBookRate(1.1);
+		bankBookDTO.setBookSale(1);
+		bankBookDTO.setBookDetail("detail1");
+		
+		int result = bankBookDAO.setBankBookAdd(bankBookDTO);
+		
+		assertEquals(1, result);
+	}
 }
