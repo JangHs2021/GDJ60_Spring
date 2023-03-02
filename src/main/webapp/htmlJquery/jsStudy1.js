2e85738f69f62609cef20e0b06e53870ffb20641
JangHyenSeo <wkdgustj02@naver.com>
2023-03-02 17:43:25 +0900

    1

---
+++ src/main/webapp/htmlJquery/jsStudy1.js
@@ -0,0 +1,19 @@
+// const btn1 = document.getElementById("btn1");
+const btn1 = document.querySelector("#btn1");
+
+// querySelector == querySelectorAll[0];
+
+// const buttons = document.getElementsByClassName("buttons");
+const buttons = document.querySelectorAll(".buttons");
+
+const d1 = document.querySelector("#d1 > .buttons li");
+
+btn1.addEventListener("click", function(){
+    console.log("btn1");
+});
+
+for(let b of buttons){
+    b.addEventListener("click", function(){
+        console.log("buttons");
+    });
+};
