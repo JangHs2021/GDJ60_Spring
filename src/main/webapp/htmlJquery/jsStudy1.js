abcd9d3e3314f26355841c3f0df74679653b0116
JangHyenSeo <wkdgustj02@naver.com>
2023-03-03 11:20:02 +0900

    1

--- src/main/webapp/htmlJquery/jsStudy1.js
+++ src/main/webapp/htmlJquery/jsStudy1.js
@@ -12,8 +12,42 @@ btn1.addEventListener("click", function(){
     console.log("btn1");
 });
 
-for(let b of buttons){
-    b.addEventListener("click", function(){
-        console.log("buttons");
+// for(let b of buttons){
+//     b.addEventListener("click", function(){
+//         console.log("buttons");
+//     });
+// };
+
+let ar = [1, 2, 3];
+
+console.log('old');
+for(let i = 0; ar.length; i++) {
+    console.log(ar[i]);
+}
+
+console.log('For of');
+for(let a of ar) {
+    console.log(a);
+}
+
+console.log('For in');
+for(let j in ar) {
+    console.log(j);
+    console.log(ar[j]);
+}
+
+console.log('For each');
+ar.forEach(function(v, i, list){
+    console.log('V : ', v, 'I : ', i, 'List : ', list);
+});
+
+alert(buttons);
+alert(ar);
+console.log(buttons);
+console.log(ar);
+
+buttons.forEach(function(v, i){
+    v.addEventListener("click", function(){
+        console.log('click');
     });
-};
+;})
